a329e9cd98cb1c167968a1f9d89f2fd94c2181a6
hulk89 <snuboy89@gmail.com>
2021-07-03 22:49:41 +0900

    plotly.js 3D viewer added. state deepcopy added

--- src/app.js
+++ src/app.js
@@ -1,61 +1,46 @@
-/*
-let src_box = [[0, 5, 5, 0], [0, 0, 5, 5]]
-let width = 30
-
-let transpose = (box) => box[0].map((_, colIndex) => box.map(row => row[colIndex]))
-let transform_view = (box, width) => box.map((point) => point.map((p, colIndex) => {
-  if (colIndex == 0) return p
-  else return width-p}
-))
-
-function draw(svg, box, color='blue') {
-  let t_box = transform_view(transpose(box), width)
-  const polygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
-  polygon.setAttributeNS(null, 'points', t_box.flat().join(' '))
-  polygon.setAttributeNS(null, 'fill', color)
-  polygon.setAttributeNS(null, 'stroke', color)
-  polygon.setAttributeNS(null, 'fill-opacity', '50%')
-  svg.append(polygon)
-  
-  let colors = ['red', 'black', 'gray', 'orange']
-  for (let i = 0 ; i < 4 ; i++) {
-    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-    circle.setAttributeNS(null, 'cx', t_box[i][0])
-    circle.setAttributeNS(null, 'cy', t_box[i][1])
-    circle.setAttributeNS(null, 'r', 1)
-    circle.setAttributeNS(null, 'stroke', colors[i])
-    svg.append(circle)
-  }
-}
-*/
 import Equation from "./components/equation.js"
 import InputMatrix from "./components/matrix.js"
-import {matrix_to_latex} from "./utils/array_to_latex.js"
-import {det} from "./utils/matrix.js"
+import Plot from "./components/plot.js"
 
+import {matrix_to_latex} from "./utils/array_to_latex.js"
+import {det, transpose} from "./utils/matrix.js"
 
 class App {
   constructor (target) {
-    let eq = document.createElement('div')
-    let mat = document.createElement('div')
+    let plot_div = document.createElement('div')
+    let eq_div = document.createElement('div')
     let det_div = document.createElement('div')
+    let mat_div = document.createElement('div')
 
-    target.appendChild(eq)
+    target.append(plot_div)
+    target.appendChild(eq_div)
     target.append(det_div)
-    target.appendChild(mat)
+    target.appendChild(mat_div)
     
-    let eq_obj = new Equation(eq)
-    let input_matrix = new InputMatrix(mat)
+    let plot = new Plot(plot_div)
+    let eq = new Equation(eq_div)
+    let input_matrix = new InputMatrix(mat_div)
 
-    let data = [[1, 0, 0],[0, 1, 0],[0, 0, 1]]
+    let data = [[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 1]]
+    
+    let arr = [[0, 0, 1, 1, 0, 0, 1, 1],
+               [0, 1, 1, 0, 0, 1, 1, 0],
+               [0, 0, 0, 0, 1, 1, 1, 1]]
+    let arr2 = [[1,1,2,2,1,1,2,2],
+                [1,2,2,1,1,2,2,1],
+                [1,1,1,1,2,2,2,2]]
 
-    eq_obj.setState({equation: matrix_to_latex(data)})
+    let plot_data = [{data:arr, color:'blue'}, {data:arr2, color:'green'}]
+    console.log(plot_data)
+
+    plot.setState({data:plot_data, title:"3D view"})
+    eq.setState({equation: matrix_to_latex(data)})
     det_div.innerHTML = `determinant of matrix: ${det(data)}`
 
     input_matrix.setState({
         data: data, 
         callback: (data) => {
-          eq_obj.setState({equation: matrix_to_latex(data)})
+          eq.setState({equation: matrix_to_latex(data)})
           det_div.innerHTML = `determinant of matrix: ${det(data)}`
         }
       })
@@ -65,37 +50,3 @@ class App {
 let target = document.getElementById('app')
 new App(target)
 
-/*
-let svg = document.getElementById('plot')
-let src = document.getElementById('src')
-
-
-let apply_btn = document.getElementById('apply-btn')
-apply_btn.addEventListener('click', () => {
-  let matrix = [[document.getElementById('1').value,         
-                 document.getElementById('2').value],
-                [document.getElementById('3').value,
-                 document.getElementById('4').value]]
-  
-  let transformed_box = matmul(matrix, src_box)
-  
-  let det = document.getElementById('det')
-  
-  
-  det.innerText = `determinant of A = $ ${matrix[0][0]} \\times ${matrix[1][1]} - ${matrix[0][1]} \\times ${matrix[1][0]} = ${matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]} $`
-  MathJax.Hub.Queue(["Typeset", MathJax.Hub, det])
-  
-  let dst = document.getElementById('dst')
-  
-  dst.innerText = 'translated box: ' + '$' + 
-    to_matrix_latex_form(matrix) + '\\times' + to_matrix_latex_form(src_box) + '=' + to_matrix_latex_form(transformed_box)+ '$'
-  MathJax.Hub.Queue(["Typeset", MathJax.Hub, dst])
-
-  while (svg.firstChild) {
-    svg.firstChild.remove()
-  }
-  
-  draw(svg, src_box)
-  draw(svg, transformed_box, color='green')
-})
-*/
---
+++ src/components/plot.js
@@ -0,0 +1,28 @@
+import Component from "../core/Component.js"
+
+import Plotly from 'plotly.js/dist/plotly.min'
+
+function to_data(array, color='blue') {
+  return {
+    x: array[0],
+    y: array[1],
+    z: array[2], 
+    i: [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+    j: [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+    k: [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+    color: color,
+    opacity: 0.8,
+    type: 'mesh3d'}
+}
+
+export default class Plot3D extends Component {
+  setup () {
+    this.$state = {data:[], height: 480, width: 480, title: ""}
+  }
+  render () {
+    let {data, height, width, title} = this.$state
+    let layout = {}
+    layout = {...layout, ...{height, width, title}}
+    Plotly.newPlot(this.$target, data.map((d) => to_data(d.data,d.color)), layout) 
+  }
+}
