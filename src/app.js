f2e1d1f43c0c0514483e0d5a626198d111a3a3a9
hulk89 <snuboy89@gmail.com>
2021-07-04 16:23:57 +0900

    add examples

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import Plot from "./components/plot.js"
 import {matrix_to_latex} from "./utils/array_to_latex.js"
 import {det, matmul} from "./utils/matrix.js"
 
-/* TODO: Add state manager and add example buttons*/
 class App {
   $state = {}
 
@@ -33,11 +32,18 @@ class App {
                       [0, 1, 1, 0, 0, 1, 1, 0],
                       [0, 0, 0, 0, 1, 1, 1, 1]]
 
+    let examples = [
+      {m: [[1,0,0],[0,1,0],[0,0,1]], desc: "identity matrix"},
+      {m: [[1,0,1],[0,1,0],[1,0,1]], desc: "determinant 0(rank=2)"},
+      {m: [[1,1,1],[1,1,1],[1,1,1]], desc: "determinant 0(rank=1)"},
+    ]
+
     this.$state = {
       outlet: {
         plot: plot,
         equation: eq,
         det_div: det_div,
+        input_form: input_form,
       },
       data: {
         m: m,
@@ -53,15 +59,28 @@ class App {
           this.render()
         }
       })
+
+    let examples_div = document.createElement('div')
+    target.append(examples_div)
+    examples.forEach((ex) => {
+      let ex_button = document.createElement('button')
+      ex_button.addEventListener('click', () => {
+        this.$state.data.m = ex.m
+        this.render()
+      })
+      ex_button.innerHTML = ex.desc
+      examples_div.append(ex_button)
+    })
   }
   render() {
-    let {plot, equation, det_div}  = this.$state.outlet
+    let {plot, equation, det_div, input_form}  = this.$state.outlet
     let {m, src_points} = this.$state.data
 
     let dst_points = matmul(m, src_points)
 
     let plot_data = [{data:src_points, color:'blue'}, {data:dst_points, color:'green'}]
 
+    input_form.setState({data: m})
     plot.setState({data:plot_data, title:"3D view"})
     equation.setState({equation: `${matrix_to_latex(dst_points)} = ${matrix_to_latex(m)} \\times ${matrix_to_latex(src_points)}`})
     det_div.innerHTML = `determinant of matrix: ${det(m)}`
